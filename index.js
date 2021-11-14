3c73a5c834ea8e1c74df0a2e08f4c691662ed5a2
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2021-11-14 13:07:44 -0500

    Added the function for the button

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 let femaleAkanName = ["Akosua" ,"Adwoa","Abeena","Akua", "Yaa","AFua","Ama"]
 let maleAkanName =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-let days = ["Sunday","Monday","Tuesday","Thursday","Friday","Saturday"]
+let days = ["Sunday","Monday","Tuesday", "Wednesday","Thursday","Friday","Saturday"]
 
-document.getElementById('forms').addEventListener('submit', akanGenerate());
-const dateInput = document.getElementById('date')
-const genderInput = document.getElementById('gender')
+
+forms.addEventListener('submit',akanGenerate);
+const dateInput = document.getElementById('date');
+const genderInput = document.getElementById('gender');
 
 
 function akanGenerate(event) {
-  // prevents default brhaviour of how the browser behaves ie refreshes itself
+ 
+  // prevents default behaviour of how the browser behaves ie refreshes itself
 event.preventDefault()
-// New date of soecific value user will put//
+// New date of specific value user will put//
 let d = new Date(dateInput.value)
 let day = days[d.getDay()]
-console.log(day)
-alert(day)
-
-// if(gender == " "|| date ==" "){
-//   console.error("Error! You have to input both your Gender and Date of Birth fpor you to view your Akan Name")
-// }
-
+if (day == "Saturday" && genderInput.value == "Male"){
+  alert("Your Akan Name is KWAME");
+} else if(day == "Saturday" && genderInput.value == "Female"){
+  alert("Your Akan Name is AMA");
+}
+if(dateInput.value == ''){
+  alert("You are required to input your dob ")
+}
 }
